261c0f47deb7d4855cd13cfac8e3d971afc1359d
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-29 22:18:27 +0200

    Task 1 Fix: 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -17,7 +17,8 @@ def index_range(page: int, page_size: int) -> tuple:
     return start_index, end_index
 
 class Server:
-    """Server class to paginate a database of popular baby names.
+    """
+    Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
@@ -36,8 +37,10 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and page > 0, "Page must be a positive integer"
-        assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive integer"
+        assert isinstance(page, int) and page > 0,\
+                "Page must be a positive integer"
+        assert isinstance(page_size, int) and page_size > 0,\
+                "Page size must be a positive integer"
 
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
