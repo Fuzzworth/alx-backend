91599abad346bed4b854341632eaee5a8d832505
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-29 22:19:22 +0200

    Task 1 Fix: 3

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -38,10 +38,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and page > 0,
-            "Page must be a positive integer"
-        assert isinstance(page_size, int) and page_size > 0,
-            "Page size must be a positive integer"
+        assert isinstance(page, int) and page > 0, "Page must be a positive integer"
+        assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive integer"
 
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
